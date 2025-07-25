32bfd14b354697be898e5f225a3a271a0476877c
Vyom <vyomj.home@gmail.com>
2025-07-24 19:03:35 -0700

    v1.0(Initializing dataset)

---
+++ gameRecommender.py
@@ -0,0 +1,54 @@
+### Dataset ###
+ratings = {
+    'EntyXD': {
+        'Minecraft': 5,
+        'Terraria': 4.5,
+        'Super Mario 64': 3,
+        'GTA 5': 4,
+        'Call of Duty': 4.5
+    },
+    'EnderKnight': {
+        'Minecraft': 5,
+        'Terraria': 3,
+        'Super Mario 64': 5,
+        'GTA 5': 4,
+        'Call of Duty': 3
+    },
+    'Nallarku':{
+         'Minecraft': 4.5,
+        'Terraria': 4,
+        'Super Mario 64': 5,
+        'GTA 5': 5,
+        'Call of Duty': 3.5
+    },
+    'Ayaan': {
+         'Minecraft': 5,
+        'Terraria': 5,
+        'Super Mario 64': 5,
+        'GTA 5': 1,
+        'Call of Duty': 1
+    },
+    'Dacoder': {
+         'Minecraft': 5,
+        'Terraria': 3,
+        'Super Mario 64': 4,
+        'GTA 5': 3.5,
+        'Call of Duty': 3
+    },
+    'ThunderBeast': {
+         'Minecraft': 5,
+        'Terraria': 2,
+        'Super Mario 64': 2.5,
+        'GTA 5': 4.5,
+        'Call of Duty': 4
+    },
+    'Marco': {
+         'Minecraft': 5,
+        'Terraria': 3,
+        'Super Mario 64': 3.5,
+        'GTA 5': 4,
+        'Call of Duty': 2.5
+    },
+}
+
+### Import libraries ###
