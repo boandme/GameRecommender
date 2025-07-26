1008f93c94c8cc4f8c7d457311391346d24d14fa
Vyom <vyomj.home@gmail.com>
2025-07-25 22:17:55 -0700

    v0.3(Similarity Rankings to a specific person)

--- gameRecommender.py
+++ gameRecommender.py
@@ -28,7 +28,7 @@ ratings = {
         'GTA 5': 1,
         'Call of Duty': 1
     },
-    'Dacoder': {
+    'Cychrone': {
          'Minecraft': 5,
         'Terraria': 3,
         'Super Mario 64': 4,
@@ -49,6 +49,74 @@ ratings = {
         'GTA 5': 4,
         'Call of Duty': 2.5
     },
+    'Ronit': {
+         'Minecraft': 5,
+        'Terraria': 4,
+        'Super Mario 64': 2,
+        'GTA 5': 3,
+        'Call of Duty': 3
+    },
 }
+### Libraries
+from math import sqrt
 
-### Import libraries ###
+### Code here ####
+def sim_distance(prefs, person1, person2):
+    shared_items = {}
+    ## Get list of shared items ###
+    for item in prefs[person1]:
+        if item in prefs[person2]:
+            shared_items[item]=1
+
+    ## if no ratings in common, return 0
+    if len(shared_items) == 0:
+        return 0
+    
+    ### Find the similarity score between two ppl ## 
+    sum_of_squares = 0
+    for item in shared_items:
+        diff = prefs[person1][item] - prefs[person2][item]
+        sum_of_squares += diff * diff
+
+    return 1/(1+sqrt(sum_of_squares))
+
+print(f"The  similarity score between EnderKnight and Cychrone is  {sim_distance(ratings, 'EnderKnight', 'Cychrone')}")
+
+
+### Sorting the reviews ####
+def topMatches(prefs, person, n=5, similarity=sim_distance):
+    scores = []  
+    for other in prefs:
+        if other != person:
+            
+            scores.append((similarity(prefs, person, other), other))
+    scores.sort()     
+    scores.reverse()
+    print()
+    print(f"--------- VG Prefs Similarity Rankings to {person}")
+    for i in range(0, len(scores)):
+        print(f"{i}: {scores[i]}")   
+    return scores
+ 
+
+name = input("For who do you want to see the similarity leaderboard for VG preferences? ")
+if name in ratings:
+    topMatches(ratings, name)
+else: 
+    print("Invalid Input")
+
+
+
+### Recommendations code: 
+## Get recommendations for a person by using weighted average
+def getRecommendations(prefs, person, similarity = sim_distance):
+    totals = {}
+    simSums={}
+    
+
+   
+
+        
+
+
+
